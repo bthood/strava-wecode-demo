9029d919aa7d4905442d5f4c8d8b59f5e77b2f12
Byron Hood <bhood0@gmail.com>
2015-02-07 12:53:07 -0500

    Inital app commit

---
+++ app/src/main/java/com/strava/segmentexplorerdemo/SegmentListActivity.java
@@ -0,0 +1,14 @@
+package com.strava.segmentexplorerdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SegmentListActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segment_list);
+    }
+
+}
